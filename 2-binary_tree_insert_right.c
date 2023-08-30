5770b0c51069504cf256fbf5e7365587228b3242
Alexus91 <mrfadlkamal@gmail.com>
2023-08-30 10:28:45 -0700

    3. Delete

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -9,22 +9,21 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-    binary_tree_t *nw_node;
+	binary_tree_t *nw_node;
 
-    if (parent == NULL)
-        return (NULL);
+	if (parent == NULL)
+		return (NULL);
 
-    nw_node = binary_tree_node(parent, value);
-    if (nw_node == NULL)
-        return (NULL);
+	nw_node = binary_tree_node(parent, value);
+	if (nw_node == NULL)
+		return (NULL);
 
-    if (parent->right != NULL)
-    {
-        nw_node->right = parent->right;
-        parent->right->parent = nw_node;
-    }
+	if (parent->right != NULL)
+	{
+		nw_node->right = parent->right;
+		parent->right->parent = nw_node;
+	}
 
-    parent->right = nw_node;
-
-    return (nw_node);
+	parent->right = nw_node;
+	return (nw_node);
 }

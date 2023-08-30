8acae935fda375f47fc8f3f83a0547929e80ee2d
Alexus91 <mrfadlkamal@gmail.com>
2023-08-30 08:39:08 -0700

    0. New node  is ready to be pushed

--- binary_trees.h
+++ binary_trees.h
@@ -26,6 +26,7 @@ struct binary_tree_s
 	struct binary_tree_s *right;
 };
 
+
 /* structs */
 typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s bst_t;
@@ -33,6 +34,7 @@ typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
 /* mandatory */
+void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);

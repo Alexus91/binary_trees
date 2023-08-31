eef4773e0c42e8e70e5b4b06e9b3fdc397d1e3b2
Alexus91 <mrfadlkamal@gmail.com>
2023-08-31 09:42:42 -0700

    adding leaf function in code to fix the ouput

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,5 +1,17 @@
 #include "binary_trees.h"
 /**
+ * binary_tree_is_leaf - checks if a node is a leaf
+ * @node: pointer to the node to check
+ *
+ * Return: 1 if node is a leaf, and 0 otherwise. If node is NULL, return 0
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+	return (node->left == NULL && node->right == NULL);
+}
+/**
  * binary_tree_height - Measures the height of a binary tree
  * @tree: Pointer to the root node of the tree to measure the height
  * Return: The height of the binary tree, or 0 if tree is NULL
@@ -8,7 +20,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_ht, right_ht;
 
-	if (tree == NULL)
+	if (tree == NULL|| binary_tree_is_leaf(tree))
 		return (0);
 
 	left_ht = binary_tree_height(tree->left);

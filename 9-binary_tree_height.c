627466a5ffbd4d40d6f047938b86242e5a41467c
Alexus91 <mrfadlkamal@gmail.com>
2023-08-31 07:41:40 -0700

    fixing  8. Post-order traversal  betty and creating  9. Height

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_height - Measures the height of a binary tree
+ * @tree: Pointer to the root node of the tree to measure the height
+ * Return: The height of the binary tree, or 0 if tree is NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_ht, right_ht;
+
+	if (tree == NULL)
+		return (0);
+
+	left_ht = binary_tree_height(tree->left);
+	right_ht = binary_tree_height(tree->right);
+
+	if (left_ht > right_ht)
+		return (left + 1);
+	else
+		return (right_ht);

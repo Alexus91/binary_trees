7bf386b7bef8b8279e7333b69d69c30e20bc1ac0
Alexus91 <mrfadlkamal@gmail.com>
2023-08-31 17:18:17 -0700

    fixing bettystyle fir task

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -35,15 +35,15 @@ size_t binary_tree_height(const binary_tree_t *tree)
  * @tree: pointer to the root node
  * Return: balance
  */
-int binary_tree_balance(const binary_tree_t *tree) 
+int binary_tree_balance(const binary_tree_t *tree)
 {
 	int l, r;
-	
-	if (tree == NULL) 
+
+	if (tree == NULL)
 		return (0);
 
 	l = binary_tree_height(tree->left);
 	r = binary_tree_height(tree->right);
-	
+
 	return (l - r);
 }
